5f3ecf97a680c42565a34fd3e0120675ad8d0b16
Imoleayo <imolee98@gmail.com>
2025-03-28 19:33:20 +0100

    Refactor App component to improve formatting and update return statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return ()
-    
-      
+  return (
+    <div>
+      my name is <strong>john doe</strong>
+    </div>
+  );
 }
 
-export default App
+export default App;
