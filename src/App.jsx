c1f69648858825920761de41a3efc0d486ffb418
Imoleayo <imolee98@gmail.com>
2025-03-31 12:46:08 +0100

    Fix formatting issues in LandPage-Style.css and refactor App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./Components/Loginpage.jsx";
 import LandPage from "./Components/LandPage.jsx";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  <Routes>
-    <Route path="/" element={<LandPage />} />
-    <Route path="/login" element={<LoginPage />} />
-  </Routes>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+};
 
 export default App;
